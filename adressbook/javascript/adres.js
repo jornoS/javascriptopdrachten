8e226b31952ccd2633909e54b49a42abd7cfc5b5
jornoS <35764708+jornoS@users.noreply.github.com>
2018-10-05 10:49:21 +0200

    version 1 of my javascript assingments

---
+++ adressbook/javascript/adres.js
@@ -0,0 +1,69 @@
+let personen = [];
+
+fetch('https://randomuser.me/api/?results=10')
+.then(function(response) {
+  return response.json();
+})
+.then(function(myJson) {
+  
+  personen = myJson.results.sort((a, b) => {
+    return (a.name.first > b.name.first) ? 1 : ((b.name.first > a.name.first) ? -1 : 0);
+  });
+  for(i = 0; i < myJson.results.length; i++){
+    personen[i] = {firstname: personen[i].name.first, 
+    lastname: personen[i].name.last,
+    image: personen[i].picture.thumbnail, 
+    email: personen[i].email, 
+    phone: personen[i].phone,
+    street: personen[i].location.street,
+    city: personen[i].location.city,
+    state: personen[i].location.state}; 
+  }
+  
+  
+  console.log(personen);
+  
+    let infoDiv = document.querySelector('.info');
+    let body = document.querySelector("body");
+    let div = document.querySelector(".contacts");
+    for (i = 0; i < personen.length; i++){
+      let user = document.createElement("div");
+      let image = document.createElement("img");
+      let fullname = document.createElement("h1");
+      let info = document.createElement("p");
+      
+     user.addEventListener("click", function(){
+      modal.style.display = "block";
+      infoDiv.innerHTML = '';
+      infoDiv.appendChild(info);
+     });
+     modal = document.getElementById('myModal');
+
+     user.onclick = function() {
+         modal.style.display = "block";
+
+     }
+
+     window.onclick = function(event) {
+         if (event.target == modal) {
+           modal.style.display = "none";
+         }
+     }
+      image.src = personen[i].image;
+      fullname.innerHTML=personen[i].firstname + ' ' + personen[i].lastname;
+      info.innerHTML='<i class="fa fa-envelope-o"></i>  Email: ' + personen[i].email + '<br><br>' + 
+      '<i class="fa fa-phone"></i>  Phonenumber: ' + personen[i].phone + ' <i class="fa fa-commenting"></i><br><br>' + 
+      '<i class="fa fa-home"></i> <br>' +
+      'City: ' + personen[i].city + '<br>' +
+      'State: ' + personen[i].state + '<br>' +
+      'adress: ' + personen[i].street;
+      
+      user.setAttribute('class', 'user');
+      div.setAttribute('class', 'userdiv');
+      console.log('nignog');
+
+      user.append(image);
+      user.append(fullname);
+      div.appendChild(user);
+    }    
+  });
